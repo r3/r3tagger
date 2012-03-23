d96121266116157a8e29060d6c41104b047666ae
Ryan Roler <ryan.roler@gmail.com>
2012-03-22 23:17:35 -0700

    Album tests all pass. New test files for Album tests.

--- Album.py
+++ Album.py
@@ -6,29 +6,49 @@ Date: 03.17.2012"""
 class Album(object):
     """Contains a list of songs and metadata that describes an album"""
 
-    def __init__(self, artist=None, title=None, date=None, tracks=[None]):
-        self.artist = artist
-        self.title = title
-        self.date = date
+    _supported_fields = ('artist', 'album', 'date', 'genre')
+
+    def __init__(self, tracks):
+        self.album = None
+        self.artist = None
+        self.date = None
+        self.genre = None
+
         self.tracks = tracks
-        
+
+        self._find_shared_tags()
+
+    def __call__(self):
+        """Saves update metadata for the songs in this album"""
+        for song in self:
+            song()
+
+    def __iter__(self):
+        for song in self.tracks:
+            yield song
+
     def _find_shared_tags(self):
         """Determines the fields that are shared between each of the Album's
         songs. Stored as sets in a dict keyed on the field name:
-        
-        dict(artist=
+
+        dict(artist='someArtist', album='someAlbum' ... )
         """
-        
-        tracker = dict()
 
         def add_fields_from_song(song):
-            for field in ('artist', 'title', 'date', 'tracks'):
-                tracker.setdefault(field, set()).add(getatter(song, field))
-        
-        for song in self.tracks:
-            add_fields_from_song(song)
-            
+            for field in Album._supported_fields:
+                tag = getattr(song, field).pop()
+                print tag
+                tracker.setdefault(field, set()).add(tag)
+
         def is_shared(field_set):
             return len(field_set) == 1
-            
-        return {field_name : field.pop() for field_name, field in tracker}
+
+        tracker = dict()
+
+        for song in self.tracks:
+            add_fields_from_song(song)
+
+        for name, field in tracker.items():
+            if is_shared(field):
+                tag = field.pop()
+                setattr(self, name, tag)
--- tests/test_Album.py
+++ tests/test_Album.py
@@ -3,12 +3,13 @@
 import shutil
 import os
 import tempfile
-import Album
-import Song
 
-class TestReadAlbum(ProvidesAlbum):
+from Song import Song
+from Album import Album
+
+class TestReadAlbum(object):
     """Tests the Album object"""
-    
+
     path = None
 
     # Setup/teardown methods as well as dependency injection
@@ -20,19 +21,23 @@ class TestReadAlbum(ProvidesAlbum):
         dest_path = os.path.join(tempdir, 'album')
 
         shutil.copytree(orig_path, dest_path)
-        TestAlbum.path = dest_path
+        TestReadAlbum.path = dest_path
 
-        return Album.Album([Song.Song(TestAlbum.path) for x in range(10)])
+        pattern = 'test_songs/album/{:0>2}.ogg'
+        
+        tracks = [Song(pattern.format(x)) for x in range(1,6)]
+        
+        return Album(tracks)
 
-    def teardown_album(self):
+    def teardown_album(self, album):
         """Teardown: Destroys test album and cleans up temp files"""
-        shutil.rmtree(TestAlbum.path)
+        shutil.rmtree(TestReadAlbum.path)
 
     def pytest_funcarg__album(self, request):
         """Dependency Injection: Album"""
         return request.cached_setup(self.setup_album, self.teardown_album,
                 scope='class')
-    
+
     # Test Methods
     def test_iterable(self, album):
         """Test album has five songs"""
@@ -42,15 +47,18 @@ class TestReadAlbum(ProvidesAlbum):
             count += 1
 
         assert count == 5
-        
+
     def test_title(self, album):
-        assert album.title == 'someAlbum'
-        
+        assert album.album == 'SomeAlbum'
+
     def test_artist(self, album):
-        assert album.artist == 'someArtist'
-        
+        assert album.artist == 'SomeArtist'
+
     def test_date(self, album):
         assert album.date == '2012'
-        
+
     def test_genre(self, album):
-        assert album.genre == 'someGenre'
+        assert album.genre == 'SomeGenre'
+
+    def test_save_tracks(self, album):
+        assert album() == None
