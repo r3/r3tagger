fcab867eedc8caa8533ffbe8398c0b268e81b59f
Ryan Roler <ryan.roler@gmail.com>
2012-05-10 21:33:33 -0700

    Added test case for an Mp3 file. Added a non-dummy mp3 file for testing: Robert Wilkens - I'll Go With Her, which is public domain and has acoustid fingerprints on musicbrainz. Further testing will be added for fingerprint lookups, which will be done in the controller module.

--- tests/test_Song.py
+++ tests/test_Song.py
@@ -121,3 +121,34 @@ class TestWriteSong(object):
 
             #0.7886621315192743 is the length given in manual tests
             assert near(song.length, 0.788)
+
+
+class TestAcoustid(object):
+    """Provides a test suite for ensuring that fingerprints are properly
+       generated from an mp3 file. Asserts that a newly made fingerprint
+       matches one pregenerated and stored in a file (it's a long string)
+    """
+
+    dir = None
+    song = None
+    filename = 'PublicDomainSong.mp3'
+
+    def setup_song(self):
+        """Setup: Setup copy of dummy song"""
+        TestWriteSong.dir = tempfile.mkdtemp()
+
+        orig_path = os.path.join('test_songs', TestWriteSong.filename)
+        dest_path = os.path.join(TestWriteSong.dir, TestWriteSong.filename)
+
+        shutil.copyfile(orig_path, dest_path)
+        TestWriteSong.song = dest_path
+        return Song.Song(dest_path)
+
+    def pytest_funcarg__song(self, request):
+        """Dependency Injection: Song"""
+        return request.cached_setup(self.setup_song,
+                scope='class')
+
+    def test_acoustid(self, song):
+        with open('test_songs/PublicDomainFingerprint.txt') as fingerprint:
+            assert fingerprint == song.fingerprint
