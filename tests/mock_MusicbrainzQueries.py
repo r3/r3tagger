05c27e8347dddbb62e2146969309762b87fd5595
r3 <ryan.roler@gmail.com>
2012-07-13 23:33:21 -0700

    Moved previous Musicbrainz shelve to the expected Responses shelve. Created low level response shelve to support the mock_MusicbrainzQueries.py frame. Now we inject the mock library into the musicbrainz library and assert that the lookups for each function are equal to the responses in the MusicbrainzResponses.shelve

--- tests/mock_MusicbrainzQueries.py
+++ tests/mock_MusicbrainzQueries.py
@@ -1,10 +1,42 @@
-def lookup_artist(name):
-    return {'artist': 'Robert Wilkins'}
+import shelve
+mb_responses = shelve.open('mock_MusicbrainzQueries.shelve')
 
 
-def lookup_album(name, artist=None):
-    if artist is None:
-        return {'album': 'The Original Rolling Stone'}
-    else:
-        return {'album': 'The Original Rolling Stone',
-                'artist': 'Robert Wilkins'}
+class ws():
+    class Query():
+        def getArtists(self, *args, **kwargs):
+            """Query: 'Nirvana'"""
+            return mb_responses['Nirvana Id']
+
+        def getReleaseGroups(self, *args, **kwargs):
+            """Query: 'Nevermind'"""
+            return mb_responses['Nevermind Release Groups']
+
+        def getTracks(self, *args, **kwargs):
+            """Query: 'Smells Like Teen Spirit'"""
+            return mb_responses['Teen Spirit Tracks']
+
+    class WebServiceError():
+        def __init__(self, *args, **kwargs):
+            pass
+
+    def ArtistFilter(self, *args, **kwargs):
+        return None
+
+    def ReleaseGroupFilter(self, *args, **kwargs):
+        return None
+
+    def TrackFilter(self, *args, **kwargs):
+        return None
+
+    def ReleaseGroupIncludes(self, *args, **kwargs):
+        return None
+
+    def ArtistIncludes(self, *args, **kwargs):
+        return None
+
+
+class m():
+    class Release():
+        TYPE_OFFICIAL = 1
+        TYPE_ALBUM = 2
