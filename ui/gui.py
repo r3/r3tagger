b207b557bffade780343989b436bbfac938f2306
r3 <ryan.roler@gmail.com>
2012-07-27 15:08:06 -0700

    Further design efforts. Things aren't connected, but it's looking better.

--- ui/gui.py
+++ ui/gui.py
@@ -2,8 +2,9 @@ import sys
 
 from PySide.QtCore import Qt
 from PySide.QtGui import (QMainWindow, QFileSystemModel, QHBoxLayout,
-                          QTreeView, QTableView, QApplication, QWidget,
-                          QAbstractItemView, QDockWidget)
+                          QTreeView, QApplication, QWidget, QVBoxLayout,
+                          QAbstractItemView, QDockWidget, QLabel, QLineEdit,
+                          QPushButton)
 
 
 class MainWindow(QMainWindow):
@@ -33,8 +34,24 @@ class MainWindow(QMainWindow):
         self.listing.setRootIndex(lsRoot)
         self.listing.setSelectionMode(QAbstractItemView.MultiSelection)
 
+        self.taggingGroup = QVBoxLayout()
+        for field in ("artist", "album", "date", "track", "number"):
+            horizLayout = QHBoxLayout()
+            horizLayout.addWidget(QLabel(field))
+            horizLayout.addWidget(QLineEdit())
+            self.taggingGroup.addLayout(horizLayout)
+
+        self.buttonGroup = QHBoxLayout()
+        self.buttonGroup.addWidget(QPushButton("Confirm"))
+        self.buttonGroup.addWidget(QPushButton("Cancel"))
+        self.buttonGroup.addStretch()
+
+        self.taggingGroup.addStretch()
+        self.taggingGroup.addLayout(self.buttonGroup)
+
         layout = QHBoxLayout()
         layout.addWidget(self.listing)
+        layout.addLayout(self.taggingGroup)
         central = QWidget()
         central.setLayout(layout)
         self.setCentralWidget(central)
