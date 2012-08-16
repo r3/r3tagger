e28b4678311bc8e157244deb8e2b0fd683d3aa42
r3 <ryan.roler@gmail.com>
2012-08-15 19:17:22 -0700

    Removed more useless code from ui.

--- ui/ui.py
+++ ui/ui.py
@@ -69,7 +69,7 @@ class MusicCollectionModel(QAbstractItemModel):
         self.root = AlbumNode("")
         self.headers = COLUMNS.keys()
 
-    def addAlbum(self, album, callReset=True):
+    def addAlbum(self, album):
         root = self.root
         albumNode = AlbumNode(album)
         root.insertChild(albumNode)
@@ -77,9 +77,6 @@ class MusicCollectionModel(QAbstractItemModel):
         for track in album:
             trackNode = TrackNode(track, albumNode)
             albumNode.insertChild(trackNode)
-
-        if callReset:
-            self.reset()
 
     def rowCount(self, parent):
         node = self.nodeFromIndex(parent)
